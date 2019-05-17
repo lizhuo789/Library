88688653e7563441be44e49072a4ec74bdcc51ce
lizhuo789 <917641339@qq.com>
2019-05-17 16:41:51 +0800

    二. 映入界面
    1. 第二种use think\controller(继承Controller类）
    2. 建立列表界面
    3. 添加add界面
    4. 编辑editor界面，如上

---
+++ README.md
@@ -0,0 +1,14 @@
+二. 映入界面
+1. 第二种use think\controller(继承Controller类）
+①引入命名空间
+②继承命名空间->使用方法
+感想：代码越简单越好
+2. 建立列表界面
+3. 添加add界面
+①在WWW\bick\application\admin\view\admin中admin.php建立add
+②会建立一个新的页面
+修改图片，和以前一样
+4. 编辑editor界面，如上
+
+
+注意：关键词不能乱用，修改词
---
+++ application/admin/controller/Admin.php
@@ -0,0 +1,31 @@
+<?php
+namespace app\admin\controller;
+
+class Admin
+{
+    public function lst()
+    {
+       return view();
+    }
+
+     public function add()
+    {
+       return view();
+    }
+
+    public function edit()
+    {
+       return view();
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
