8a94628f5933770e9297f3289b4f9c912a3b11b5
赵姗 <2969138926@qq.com>
2019-05-19 22:40:04 +0800

    控制器层的修改
    1.编辑和删除
    2.通过数组的方式传递信息
    3.在控制器中进行数据处理修改

--- application/config.php
+++ application/config.php
@@ -143,7 +143,9 @@ return [
     ],
 
     // 视图输出字符串内容替换
-    'view_replace_str'       => [],
+    'view_replace_str'    => [
+        '__INDEX__'=>'http://127.0.0.1/Library/public/static/index'
+    ],
     // 默认跳转页面对应的模板文件
     'dispatch_success_tmpl'  => THINK_PATH . 'tpl' . DS . 'dispatch_jump.tpl',
     'dispatch_error_tmpl'    => THINK_PATH . 'tpl' . DS . 'dispatch_jump.tpl',
